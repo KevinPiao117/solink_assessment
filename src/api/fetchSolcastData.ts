d923cc9657502bf5df88df9f8f90a2556e324062
KevinPiao117 <kevin.piao117@gmail.com>
2025-03-10 13:54:14 -0400

    added_postgres_connection

--- src/api/fetchSolcastData.ts
+++ src/api/fetchSolcastData.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
-// Define API response structure
+
 export interface SolcastData {
     period_end: string;              // Timestamp in ISO format
     air_temp: number;                // Air temperature (°C)
@@ -12,7 +14,10 @@ export interface SolcastData {
     pv_power_rooftop: number;         // PV power generation (kW)
 }
 
-const apiUrl = "https://script.googleusercontent.com/macros/echo?user_content_key=3iV3nyDOscoIQDBZ_zVXhhRj-VBAoE62NNlRx8XQjjKvLxqL9BtbHNo7m4tYuLcn7jk_5rF9D-xMeOdFwPCiwbxCHwb_GVYhOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMWojr9NvTBuBLhyHCd5hHa6ZQL9zg4nzErHkuFtBREH245O9-x8_AESik0EeAdGGaTK2AcpoR_CTlhzD6mry43fUpb2DILZnlvMGYFEwILfDvr7kDVOU7VuX2KnvcqMMGFZR6btdKBkCY8-paDNctvg&lib=MR_mt8Wmapn2W5zwbI-xTtMWO3py5UuMP";
+if (!process.env.SOLCAST_API_URL) {
+    throw new Error("Missing environment variable: SOLCAST_API_URL");
+}
+const apiUrl = process.env.SOLCAST_API_URL;
 
 /**
  * Fetch data from Solcast API
---
+++ src/database/test_db.ts
@@ -0,0 +1,14 @@
+import { pool } from "./db_helper";
+
+async function testDB() {
+    try {
+        const result = await pool.query("SELECT NOW();");
+        console.log("Successfully connected to AWS PostgreSQL:", result.rows);
+    } catch (error) {
+        console.error("Connection Failed:", error);
+    } finally {
+        pool.end();
+    }
+}
+
+testDB();
